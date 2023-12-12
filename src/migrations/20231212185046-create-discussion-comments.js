b6dd4e6f73f2f3a49e5e18b99aacff291050398e
eprayoga <endangprayoga95@gmail.com>
2023-12-13 01:55:04 +0700

    Create discussion comments model

---
+++ src/migrations/20231212185046-create-discussion-comments.js
@@ -0,0 +1,44 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('discussion_comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      discussion_id: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      comment: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      status: {
+        allowNull: false,
+        defaultValue: "active",
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('discussion_comments');
+  }
+};
---
+++ src/models/discussion_comments.js
@@ -0,0 +1,55 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class discussion_comments extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  discussion_comments.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    user_id: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    discussion_id: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    comment: {
+      allowNull: false,
+      type: DataTypes.TEXT
+    },
+    status: {
+      defaultValue: 'active',
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      defaultValue: sequelize.fn('now'),
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      defaultValue: sequelize.fn('now'),
+      type: DataTypes.DATE
+    },
+  }, {
+    sequelize,
+    tableName: 'discussion_comments',
+    modelName: 'DiscussionComments',
+  });
+  return discussion_comments;
+};
