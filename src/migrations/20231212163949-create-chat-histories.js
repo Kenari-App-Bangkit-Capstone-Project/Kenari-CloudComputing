edffe39793e978cab50b4763831cea53ec886653
eprayoga <endangprayoga95@gmail.com>
2023-12-12 23:58:23 +0700

    Create chatbot user histories model

---
+++ src/migrations/20231212163949-create-chat-histories.js
@@ -0,0 +1,32 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('chat_histories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      user_input: {
+        type: Sequelize.STRING
+      },
+      response: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.fn('now'),
+        type: Sequelize.DATE
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('chat_histories');
+  }
+};
---
+++ src/models/chat_histories.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class chat_histories extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  chat_histories.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    user_id: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    user_input: {
+      type: DataTypes.STRING
+    },
+    response: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      defaultValue: sequelize.fn('now'),
+      type: DataTypes.DATE
+    },
+  }, {
+    sequelize,
+    timestamps: false,
+    tableName: 'chat_histories',
+    modelName: 'ChatHistories',
+  });
+  return chat_histories;
+};
